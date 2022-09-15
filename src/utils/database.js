ca199b79c74c083d0c5326a8f067b8485c88fa10
duvanca <73137805+duvanca@users.noreply.github.com>
2022-09-15 10:04:50 -0500

    second commit

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,13 +1,26 @@
 const { Sequelize } = require('sequelize');
 
+const dotenv = require('dotenv');
+
+// Create a connection to database
+dotenv.config({ path: './config.env' });
 // Create a connection to database
 const db = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'root',
-  database: 'Airbnb',
-  port: 5432
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
+  logging: false,
+  dialectOptions:
+    process.env.NODE_ENV === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
 });
 
 module.exports = { db }
